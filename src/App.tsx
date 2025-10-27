7401e9c208c45a3aa1c76cbdfcb3aa81d77df92f
Ahmad Zainul Mufid <ahmadzainulmufid12@gmail.com>
2025-10-27 20:14:58 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">
+        Welcome KunFayakun-MIA2025 🌙
+      </h1>
+    </div>
+  );
+}
